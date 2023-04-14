76235e811f366aa5e35b9bea0d8e5134de865d66
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-04-14 14:22:08 +0800

    命名路由相关代码提交

---
+++ vue_test/33_src_命名路由/pages/Detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li>消息编号：{{this.$route.query.id}}</li>
+    <li>消息标题：{{this.$route.query.title}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+    name: 'Detail',
+    // 在钩子里声明接收来自Message的query参数
+    mounted() {
+      console.log(this);
+    },
+}
+</script>
+
+<style>
+
+</style>

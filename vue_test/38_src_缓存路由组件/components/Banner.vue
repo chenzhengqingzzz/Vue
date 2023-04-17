938e28520efe7d3898f69ca1a8f0d587635b0fa6
陈正清macbook pro <tenchenzhengqing@qq.com>
2023-04-17 16:29:40 +0800

    编程式路由导航和缓存路由组件相关代码提交（是的，我又忘记备份src代码了qaq打自己一拳）

---
+++ vue_test/38_src_缓存路由组件/components/Banner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-xs-offset-2 col-xs-8">
+    <div class="page-header">
+      <h2>Vue Router Demo</h2>
+      <button @click="back">后退</button>
+      <button @click="forword">前进</button>
+      <button @click="test">测试一下go</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  methods: {
+    back(){
+      this.$router.back()
+    },
+    forword(){
+      this.$router.forword()
+    },
+    test(){
+      this.$router.go(-2)
+    }
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue_test/38_src_缓存路由组件/pages/Message.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in messageList" :key="item.id">
+        <!-- 跳转路由并携带query参数，to的字符串写法 -->
+        <!-- <router-link :to="`/home/message/detail?id=${item.id}&title=${item.title}`">{{item.title}}</router-link>&nbsp;&nbsp; -->
+        <!-- 跳转路由并携带query参数，to的对象写法 -->
+        <!-- <router-link :to="{
+          name: 'xiangqing',
+          query: {
+            id: item.id,
+            title: item.title
+          }
+        }">
+          {{item.title}}
+        </router-link>&nbsp;&nbsp; -->
+
+        <!-- 跳转路由并携带params参数，to的字符串写法 -->
+        <!-- <router-link :to="`/home/message/detail/${item.id}/${item.title}`">{{item.title}}</router-link>&nbsp;&nbsp; -->
+        <!-- 跳转路由并携带params参数，to的对象写法 -->
+        <router-link :to="{
+          // params参数里，下面只能写name，如果写path传入完整路径会报错
+          name: 'xiangqing',
+          params: { 
+            id: item.id,
+            title: item.title,
+            age: '' || undefined
+          }
+        }">
+          {{item.title}}
+        </router-link>&nbsp;&nbsp;
+        <button @click="pushShow(item)">push查看</button>
+        <button @click="replaceShow(item)">replace查看</button>
+      </li>
+    </ul>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data(){
+    return {
+      messageList: [
+        {id: '001', title: '消息001'},
+        {id: '002', title: '消息002'},
+        {id: '003', title: '消息003'},
+        ]
+    }
+  },
+  methods: {
+    pushShow(item){
+      this.$router.push({
+        name: 'xiangqing',
+        params: {
+          id: item.id,
+          title: item.title,
+          age: "" || undefined
+        }
+      })
+    },
+    replaceShow(item){
+      this.$router.replace({
+        name: 'xiangqing',
+        params: {
+          id: item.id,
+          title: item.title,
+          age: "" || undefined
+        }
+      })
+    }
+  },
+};
+</script>
+
+<style>
+</style>
